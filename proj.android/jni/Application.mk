01680d033d8a8e3b953dce6a4b2f5b67f566b9cd
slimek <slime573@gmail.com>
2014-11-06 00:23:53 +0800

    [Android] Change APP_STL to c++_static, and BRITTLE_CONFIG has gcc48.

--- proj.android/jni/Application.mk
+++ proj.android/jni/Application.mk
@@ -1,26 +1,30 @@
 APP_PLATFORM := android-15
 APP_ABI := x86 armeabi
-APP_STL := gnustl_static
 
-APP_CFLAGS := -fexceptions
-APP_CPPFLAGS := -std=c++11 -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=1
+APP_STL := c++_static
+NDK_TOOLCHAIN_VERSION := 4.8
+
+APP_CFLAGS := -fexceptions -D__STDC_LIMIT_MACROS
+APP_CPPFLAGS := \
+	-std=c++11 -frtti -fsigned-char -Wno-extern-c-compat \
+	-DCC_ENABLE_CHIPMUNK_INTEGRATION=1
+
 
 ifeq ($(NDEBUG),1)
 
 # Release
 APP_OPTIM := release
-COCOS_CONFIG := Release
-BRITTLE_CONFIG := Release
+BRITTLE_CONFIG := gcc48.Release
 
 else
 
 # Debug
 APP_OPTIM := debug
-COCOS_CONFIG := Debug
-BRITTLE_CONFIG := Debug
+BRITTLE_CONFIG := gcc48.Debug
+
 APP_CFLAGS += -g -DCOCOS2D_DEBUG=1
 
 endif
 
-NDK_APP_OUT := obj/$(COCOS_CONFIG)
-NDK_TOOLCHAIN_VERSION := 4.8
+NDK_APP_OUT := obj/$(BRITTLE_CONFIG)
+
